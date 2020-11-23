7b99856dc875ff6697fe7aa9f8aa6d4a063e3ffb
aeoluscherokee <71491295+aeoluscherokee@users.noreply.github.com>
2020-11-23 21:35:09 +0700

    create repo for python practice

--- DataTH-DataEngineer/Workshop1/config.py
+++
@@ -1,6 +0,0 @@
-MYSQL_HOST = 'db4free.net'
-MYSQL_PORT = 3306  # default port ของ MySQL คือ 3306
-MYSQL_USER = 'road2de2'
-MYSQL_PASSWORD = 'test1234'
-MYSQL_DB = 'road2de2'
-MYSQL_CHARSET = 'utf8mb4'
--- DataTH-DataEngineer/stocks/config.py
+++
@@ -1,6 +0,0 @@
-MYSQL_HOST = 'database-1.cwtxlmv0hg6n.ap-southeast-1.rds.amazonaws.com'
-MYSQL_PORT = 3306  # default port ของ MySQL คือ 3306
-MYSQL_USER = 'aeoluscherokee'
-MYSQL_PASSWORD = 'Pierre11'
-MYSQL_DB = 'stocks'
-MYSQL_CHARSET = 'utf8mb4'
